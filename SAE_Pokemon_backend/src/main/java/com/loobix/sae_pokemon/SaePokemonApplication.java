8fafc4243dc9360166fcbbcb603c1bd3b7214d72
Loobixx <148224885+Loobixx@users.noreply.github.com>
2026-01-10 11:35:51 +0100

    Initialisation du projet front + back

---
+++ SAE_Pokemon_backend/src/main/java/com/loobix/sae_pokemon/SaePokemonApplication.java
@@ -0,0 +1,13 @@
+package com.loobix.sae_pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaePokemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaePokemonApplication.class, args);
+    }
+
+}
---
+++ SAE_Pokemon_backend/src/test/java/com/loobix/sae_pokemon/SaePokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loobix.sae_pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaePokemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
